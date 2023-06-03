443885b7f386170879d2eb7c1aadd22cddb48d3f
Raj Sahu <sahuraj457@gmail.com>
2023-06-03 23:43:44 +0530

    code written

---
+++ weather.py
@@ -0,0 +1,48 @@
+import requests
+import sys
+import dotenv
+import os
+
+# Load environment variables
+dotenv.load_dotenv()
+
+# Get weather data from OpenWeatherMap API
+def get_weather_data(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        weather_data =  response.json()
+        return weather_data
+    else:
+        # print(f'\033[91mError: {response.status_code} \033[00m')
+        sys.exit(1)
+
+def parse_weather(weather_data):
+    try:
+        weather = weather_data['weather'][0]['main']
+        temperature = weather_data['main']['temp']
+        humidity = weather_data['main']['humidity']
+        wind_speed = weather_data['wind']['speed']
+        
+        print(f'*****{weather_data["name"]}*****')
+        print(f'Weather: {weather}')
+        print(f'Temperature: {temperature}')
+        print(f'Humidity: {humidity}')
+        print(f'Wind Speed: {wind_speed}')
+    except:
+        print('\033[91mError: Invalid response from the API.\033[00m')
+        sys.exit(1)
+        
+        
+def main():
+    if len(sys.argv) < 2:
+        print('\033[91mError: Please provide a city name.\033[00m')
+        sys.exit(1)
+    
+    city = sys.argv[1]
+    api_key = os.getenv('API_KEY')
+    weather_data = get_weather_data(city, api_key)
+    parse_weather(weather_data)
+    
+if __name__ == '__main__':
+    main()
